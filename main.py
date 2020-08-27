515a6ab2cf395b306bfe1e2bfd1488c45c69c87d
Wenjun Ju <wkj5070@psu.edu>
2020-08-27 16:32:00 +0000

    degree

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 a = input ("Enter temperature in celsius: ")
 a = float (a)
 b = 32+ (9/5)* a 
-print (f"{str(a)}° in Celsius is equivalent to {str(b)}° Fahrenheit. ")
+print (f"{str(a)} \N{degree sign} in Celsius is equivalent to {str(b)} \N{degree sign} Fahrenheit. ")
